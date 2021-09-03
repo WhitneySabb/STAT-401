a6231dab70d133c9c96c6949942e7bd1098fc3de
WhitneySabb <89420814+WhitneySabb@users.noreply.github.com>
2021-09-03 16:48:19 -0400

    Add files via upload

---
+++ September 3.R
@@ -0,0 +1,27 @@
+# Author: Whitney Sabb, Date: Sept 3 2021, Purpose: Perform ANNOVA on Sample Data
+
+library(dplyr) 
+
+PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv" 
+
+df <- read.csv(PATH) %>% select(-X) %>% mutate(poison = factor(poison, ordered = TRUE))
+
+glimpse(df)
+
+levels(df$poison) 
+
+df %>% group_by(poison) %>% summarise( count_poison = n(), mean_time = mean(time, na.rm = TRUE), sd_time = sd(time, na.rm = TRUE) )
+
+install.packages("ggplot2")
+
+library(ggplot2)
+
+ggplot(df, aes(x = poison, y = time, fill = poison)) + geom_boxplot() + geom_jitter(shape = 15, color = "steelblue", position = position_jitter(0.21)) + theme_classic()
+
+anova_one_way <- aov(time~poison, data = df)
+
+summary(anova_one_way)
+
+Critical F-value:	3.20431729
+Signifcant Difference
+
